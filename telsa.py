eb0dd377ae4d8d63a1485844de09ef6d7dfad302
hb <82237720+hbbots@users.noreply.github.com>
2022-05-21 11:25:53 +0530

    Update and rename BOTL.py to telsa.py

--- telsa.py
+++ telsa.py
@@ -1,4 +1,5 @@
-from cgitb import text
+
+
 import pyrogram
 from pyrogram import Client,filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -7,7 +8,9 @@ from human import TimeFormatter
 from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent,
 InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup)
 from datetime import datetime
-
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+from io import BytesIO
+from utlis import extract_user
 import tgcrypto
 HB= Client("Echo-bot", 
             api_id = 
@@ -15,6 +18,7 @@ HB= Client("Echo-bot",
             api_hash = "45e59f574d6c0574a6cb26f82689c349",
             bot_token = "1869097232:AAHJPKELP7brFuXqq0vpmDCrZ8pA_wPTgeM"
 )
+
 
 START_TEXT = """**HI {},
 I AM A SIMPLE MEDIA INFO BOT 
@@ -52,6 +56,14 @@ START_BUTTONS = InlineKeyboardMarkup(
         ],[
         InlineKeyboardButton('🆘HELP🆘', callback_data='help'),
         InlineKeyboardButton('🤗ABOUT🤗', callback_data='about'),
+        InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
+        ]]
+    )
+ot_buttons = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📢CHANNEL📢', url='https://telegram.me/TELSABOTS'),
+        InlineKeyboardButton('🧑🏼‍💻DEV🧑🏼‍💻', url='https://telegram.me/alluaddict')
+        ],[
         InlineKeyboardButton('🔐CLOSE🔐', callback_data='close')
         ]]
     )
@@ -149,6 +161,23 @@ async def Source_message(bot, update):
     )
 
     
+@HB.on_message((filters.group | filters.private) & filters.command(["json"]))
+async def group(bot, update):
+    await reply_file(bot, update.reply_to_message)
+
+
+async def reply_file(bot, update):
+    with BytesIO(str.encode(str(update))) as file:
+        file.name = "JSON.txt"
+        await update.reply_document(
+            document=file,
+            quote=True,
+            reply_markup=ot_buttons
+        )
+        try:
+            os.remove(file)
+        except:
+            pass
 
 
 
@@ -286,6 +315,13 @@ def welcomebot(client, update):
         disable_web_page_preview=True
     )
 
+
+
+
+@HB.on_message(filters.channel & filters.text | filters.media )
+async def tag(client, message):
+ await message.copy(message.chat.id)
+ 
 @HB.on_message(filters.command(["ping"]))
 async def ping(client, message):
     start = datetime.now()
@@ -302,20 +338,21 @@ REPLY_MESSAGE_BUTTONS = [
         
     ]
 ]
-@HB.on_message(filters.command('OP'))
+button_text = "CHOOSE ANY BUTTON BELOW"
+@HB.on_message(filters.command('Kbuttons'))
 def greet(bot, message):
-    text = REPLY_MESSAGE
+    text = button_text
     reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, one_time_keyboard=True, resize_keyboard=True) 
     message.reply(
         text=text,
         reply_markup=reply_markup
     )
 
-START_TEX ="HI {}, I AM A SIMPLE MEDIA INFO BOT  BUT I CAN DO MORE"
+START_TEX ="HI  I AM A SIMPLE MEDIA INFO BOT \n\n BUT I CAN DO MORE"
 
 @HB.on_message(filters.regex("START"))
 def reply_to_START(bot,message):
-    text=START_TEX.format(message.from_user.mention),
+    text=START_TEX
     reply_markup = START_BUTTONS
     message.reply_text(
     text=text,
@@ -337,6 +374,83 @@ def reply_to_ABOUT(bot,message):
     text=text,
     reply_markup=reply_markup
     )
+
+
+
+
+@HB.on_message(filters.command(["info"]))
+async def who_is(client, message):
+    # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
+    status_message = await message.reply_text(
+        "`Fetching user info...`"
+    )
+    await status_message.edit(
+        "`Processing user info...`"
+    )
+    from_user = None
+    from_user_id, _ = extract_user(message)
+    try:
+        from_user = await client.get_users(from_user_id)
+    except Exception as error:
+        await status_message.edit(str(error))
+        return
+    if from_user is None:
+        return await status_message.edit("no valid user_id / message specified")
+    message_out_str = ""
+    message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
+    last_name = from_user.last_name or "<b>None</b>"
+    message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
+    message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
+    username = from_user.username or "<b>None</b>"
+    dc_id = from_user.dc_id or "[User Doesn't Have A Valid DP]"
+    message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
+    message_out_str += f"<b>➲User Name:</b> @{username}\n"
+    message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
+    if message.chat.type in (("supergroup", "channel")):
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>➲Joined this Chat on:</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
+            )
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        buttons = [[
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            reply_markup=reply_markup,
+            caption=message_out_str,
+            parse_mode="html",
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        buttons = [[
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=message_out_str,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode="html",
+            disable_notification=True
+        )
+    await status_message.delete()
+
 
 print("HB")
 HB.run()
